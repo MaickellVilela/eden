e529adf5e75eb24deb71768a7b730bd68866d29f
Maickell Vilela <maickell@me.com>
2019-03-18 22:42:40 -0300

    Sobrenome do Maickell Adicionado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <h1>Maickell</h1>
+    <h1>Maickell Vilela</h1>
   </div>
 </template>
 
